4955759c15862188fb919ddf63586bee0d497225
Jonathan Lewis <jonathan.lewis@welch.edu>
2025-03-06 20:53:00 -0600

    dealt with cors and preflight. Can handle clash id validation

--- server/twitch_server.go
+++ server/twitch_server.go
@@ -1,15 +1,13 @@
 package server
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"net/http"
-	// "os"
-	// "os/signal"
-	// "syscall"
-	// "time"
 
+	clash "github.com/Drybonez235/clash_royale_twitch_prediction_bot/clash_royale_api"
 	logger "github.com/Drybonez235/clash_royale_twitch_prediction_bot/logger"
 	"github.com/Drybonez235/clash_royale_twitch_prediction_bot/sqlite"
 	"github.com/Drybonez235/clash_royale_twitch_prediction_bot/twitch_api"
@@ -21,40 +19,26 @@ type Authorization_JSON struct {
 	state string
 }
 
+type Player_tag struct{
+	Player_tag string `json:"clash_id""`
+}
+
 func Start_server(logger *logger.StandardLogger) {
 	logger.Info("Started server on localhost 3000")
-	defer logger.Info("Server stopped for some reason")
 
-	// ticker := time.NewTicker(1 * time.Hour)
-	// defer ticker.Stop()
-
-	// go func () {
-	// 	for range ticker.C{
-	// 	err := verfify_tokens()
-	// 	if err!=nil{
-	// 		logger.Error(err.Error())
-	// 	}
-	// 	logger.Info("Verified tokens")
-	// 	}
-	// }()
-
-	// sigChan := make(chan os.Signal, 1)
-	// signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
-
-	// <-sigChan
 
 	redirect_uri := func(w http.ResponseWriter, req *http.Request) {
 		logger.Info("Recived an app request")
-		valid, err := proccess_authorization_form(req)
+		valid, err := process_authorization_form(req)
 
 		if err!=nil{
 			logger.Error(err.Error())
 			w.WriteHeader(http.StatusInternalServerError)
-		} 
-		
-		if !valid{
-			logger.Warn(err.Error())
+			return
+		} else if !valid{
+			logger.Warn("malicious request: State not found in db")
 			w.WriteHeader(http.StatusUnauthorized)
+			return
 		}
 
 		logger.Info("Registered a user from " + req.URL.String())
@@ -84,10 +68,19 @@ func Start_server(logger *logger.StandardLogger) {
 		logger.Info("Logged event" + req.URL.String())
 	}
 
+	verify_player_tag := func(w http.ResponseWriter, req *http.Request){
+		err := handle_verify_player_tag(w, req)
+		if err!=nil{
+			logger.Error(err.Error())
+		}
+		fmt.Println("We have been hit")
+	}
+
 
 	http.HandleFunc("/redirect", redirect_uri)
 	http.HandleFunc("/subscription_handler", subscription_callback)
 	http.HandleFunc("/receive_twitch_event", handle_event)
+	http.HandleFunc("/verify_player_tag", verify_player_tag)
 	
 
 	http.ListenAndServe("localhost:3000", nil)
@@ -111,7 +104,7 @@ func subscription_handler(req *http.Request, w http.ResponseWriter)(bool,error){
 }
 
 
-func proccess_authorization_form(req *http.Request)(bool, error){
+func process_authorization_form(req *http.Request)(bool, error){
 	fmt.Println("Proccessed auth form")
 	
 	var response Authorization_JSON
@@ -134,8 +127,7 @@ func proccess_authorization_form(req *http.Request)(bool, error){
 	}
 
 	if !valid{
-		err = errors.New("malicious request: State not found in db")
-		return false, err
+		return false, nil
 	}
 
 	err = twitch_api.Request_user_oath_token(response.code)
@@ -151,7 +143,7 @@ func event_handler(w http.ResponseWriter, req *http.Request, logger *logger.Stan
 	err := Handle_event(w, req, logger)
 
 	if err!=nil{
-		w.WriteHeader(http.StatusInternalServerError) 
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return err	
 	}
 	return nil
@@ -163,5 +155,52 @@ func verfify_tokens()(error){
 		return err
 	}
 	return nil
+}
+
+func handle_verify_player_tag(w http.ResponseWriter, req *http.Request)(error){
+
+	if req.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return nil
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	body, err := io.ReadAll(req.Body)
+	if err!=nil{
+		return err}
+
+	var string_player_tag Player_tag
+
+	err = json.Unmarshal(body, &string_player_tag)
+
+	if err!=nil{
+		return err
+	}
+
+	true_false, err := clash.Validate_user_id(string_player_tag.Player_tag)
+
+	if err!=nil{return nil}
+
+	if true_false{
+		w.Write([]byte(`{"valid":true}`))
+	} else {	
+		w.Write([]byte(`{"valid":false}`))
+	}
+	return nil
+}
+
+func handlePreflight(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Max-Age", "3600")
+	w.WriteHeader(http.StatusNoContent)
 }
  
